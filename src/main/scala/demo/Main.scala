6da945a29c4fc43337b665695dbec5c81c0b6c82
Tim Harper <timcharper@gmail.com>
2014-02-12 22:48:28 -0700

    reproduced locally

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -1,18 +1,46 @@
 package demo
 
+import org.json4s.JsonAST.{JValue,JInt,JDouble}
+
+object DirectlyCopied {
+  trait DoubleMode { self: Implicits ⇒
+    implicit def double2jvalue(x: Double): JValue = JDouble(x)
+
+  }
+  object DoubleMode extends Implicits with DoubleMode
+  trait Implicits {
+    implicit def int2jvalue(x: Int): JValue = JInt(x)
+    // ...
+  }
+}
+
+object Reimplemented {
+  object DoubleMode {
+    implicit def int2jvalue(x: Int): JValue = JInt(x)
+    implicit def double2jvalue(x: Double): JValue = JDouble(x)
+  }
+}
+
 object Main extends App {
   // Json4s exhibits the problem:
   {
-    import org.json4s.JsonAST.JValue
     {
-      import org.json4s.JsonDSL.{int2jvalue,double2jvalue}
+      import org.json4s.DoubleMode.{int2jvalue,double2jvalue}
 
       // INTELLIJ FALSE NEGATIVE!!!
       // double2jvalue and int2jvalue appear to collide, but the compiler doesn't mind
-      val value: JValue = 10
+      val value: JValue = 5
       println("JInt = %s" format value)
     }
 
+    // For some reason, if I re-implement them (with identical implementations), it works.
+    {
+      import DirectlyCopied.DoubleMode.{int2jvalue,double2jvalue}
+      val value: JValue = 5
+      println("XInt = %s" format value)
+    }
+
+    // Works
     {
       import org.json4s.JsonDSL.int2jvalue
       val value: JValue = 10
@@ -26,29 +54,5 @@ object Main extends App {
     }
   }
 
-  // I can't reproduce it without the library
-  {
-    sealed trait XValue
-    case class XInt(amount: Int) extends XValue {
-      override def toString =
-        "XInt(%s)" format amount
-    }
 
-    case class XDouble(amount: Double) extends XValue {
-      override def toString =
-        "XDouble(%s)" format amount
-    }
-
-    object Conversions {
-      implicit def int2XValue(i: Int): XValue = XInt(i)
-      implicit def double2XValue(d: Double): XValue = XDouble(d)
-    }
-
-    {
-      import Conversions.{int2XValue,double2XValue}
-      // THIS SHOULD BE A PROBLEM FOLLOWING ABOVE LOGIC, but isn't.
-      val value: XValue = 5
-      println("XInt = %s" format value)
-    }
-  }
 }
